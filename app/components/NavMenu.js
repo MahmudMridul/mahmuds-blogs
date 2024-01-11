b1cdf68a0461fd3ff0e36ef753ec69169d2fadf6
Abdullah Al Mahmud <mahmudmridul.185@gmail.com>
2024-01-11 21:28:42 +0600

    Initial setup

---
+++ app/components/NavMenu.js
@@ -0,0 +1,10 @@
+export default function NavMenu() {
+   return (
+      <nav className="bg-slate-800 text-white text-xl flex justify-center w-80 h-11 pt-2 rounded-3xl">
+         <ul className="flex gap-5">
+            <li>Home</li>
+            <li>Categories</li>
+         </ul>
+      </nav>
+   )
+}
