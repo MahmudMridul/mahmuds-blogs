071e589c8542b3734ff1f1698808b30a5bb6281f
Abdullah Al Mahmud <mahmudmridul.185@gmail.com>
2024-01-11 23:09:48 +0600

    Add categories

---
+++ app/categories/page.js
@@ -0,0 +1,20 @@
+import data from "@/public/data/data.json"
+import Link from "next/link";
+
+export default async function Categories() {
+   const categories = data.categories;
+   return (
+      <div>
+         <div className="text-2xl text-center font-bold m-6">Categories</div>
+         <ul className="text-center">
+            {
+               categories.map((category, index) => {
+                  return (
+                     <li key={index}> <Link href={`/categories/${category.slug}`}>{category.name}</Link> </li>
+                  )
+               })
+            }
+         </ul>
+      </div>
+   )
+}
