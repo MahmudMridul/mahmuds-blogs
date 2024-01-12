0c765397650a15a574f6beeacec75232e2938170
Abdullah Al Mahmud <mahmudmridul.185@gmail.com>
2024-01-12 13:30:35 +0600

    Get blogs from folder

--- app/categories/[category]/[blog]/page.js
+++ app/categories/[category]/[blog]/page.js
@@ -1,7 +1,21 @@
+import matter from "gray-matter";
+import fs from "fs";
+import Markdown from "markdown-to-jsx";
 
+function getBlogContent(category, blog) {
+   const file = `./public/blogs/${category}/${blog}.md`;
+   const content = fs.readFileSync(file, "utf-8");
+   return matter(content);
+}
 
 export default function Blog({ params }) {
+   const { category, blog } = params;
+   const content = getBlogContent(category, blog);
+   console.log(content);
    return (
-      <div>{params.blog}</div>
+      <div>
+         <div>{params.blog}</div>
+         <Markdown>{content.content}</Markdown>
+      </div>
    )
 }
--- app/categories/[category]/page.js
+++ app/categories/[category]/page.js
@@ -1,8 +1,16 @@
-import data from "@/public/data/data.json"
 import Link from "next/link";
+import fs from "fs";
+
+function getBlogMetaData(category) {
+   const folder = `./public/blogs/${category}/`;
+   const files = fs.readdirSync(folder);
+   const blogs = files.map((file) => file.replace(".md", ""));
+   return blogs;
+}
 
 export default function Category({ params }) {
-   const blogs = data.blogs;
+   const blogs = getBlogMetaData(params.category);
+   console.log(blogs);
    return (
       <div>
          <div className="text-2xl text-center font-bold m-6">{params.category}</div>
@@ -10,7 +18,7 @@ export default function Category({ params }) {
             {
                blogs.map((blog, index) => {
                   return (
-                     <li key={index}> <Link href={`/categories/${params.category}/${blog.slug}`}>{blog.title}</Link> </li>
+                     <li key={index}> <Link href={`/categories/${params.category}/${blog}`}>{blog}</Link> </li>
                   )
                })
             }
