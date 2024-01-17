82c7058d600f9a01dc6a13a55db554a92e65731b
Abdullah Al Mahmud <mahmudmridul.185@gmail.com>
2024-01-17 18:34:07 +0600

    Add list item component

---
+++ src/components/ListItem.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+export default function ListItem({ blog }) {
+   return (
+      <li className="bg-zinc-100 p-2 mt-2 rounded-md hover:bg-zinc-200">
+         <Link to={`blogs/${blog.fileName}`}>
+            <h6 className="">{blog.name}</h6>
+            <div> Category: {blog.category} </div>
+            <div> Published: {blog.published} </div>
+         </Link>
+      </li>
+   );
+}
