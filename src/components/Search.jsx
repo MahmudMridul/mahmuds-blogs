6dbbba08c467134d6a585e982ef96ee750b64960
Abdullah Al Mahmud <mahmudmridul.185@gmail.com>
2024-01-17 18:34:29 +0600

    Add search component

---
+++ src/components/Search.jsx
@@ -0,0 +1,36 @@
+import React, { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { getBlogList, setState } from "../redux/appSlice";
+import { searchBlogs } from "../lib/utils";
+
+export default function Search() {
+   const dispatch = useDispatch();
+   const states = useSelector((store) => store.app);
+   const { blogList, searchText } = states;
+
+   useEffect(() => {
+      if (searchText.length !== 0) {
+         let filteredList = searchBlogs(searchText, blogList);
+         dispatch(setState("blogList", filteredList));
+      } else if (searchText.length === 0) {
+         dispatch(getBlogList());
+      }
+   }, [searchText]);
+
+   function handleSearch(event) {
+      const text = event.target.value;
+      dispatch(setState("searchText", text));
+   }
+
+   return (
+      <div className="flex justify-end m-4 ">
+         <input
+            type="text"
+            className="border-2 border-slate-300 rounded-md w-96 h-10 font-semibold text-lg"
+            placeholder="Search by name or category"
+            value={searchText}
+            onChange={handleSearch}
+         ></input>
+      </div>
+   );
+}
