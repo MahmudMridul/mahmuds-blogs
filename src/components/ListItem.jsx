7f7a11f9b41efaede0ee7674e4a0588bc7f44a67
Abdullah Al Mahmud <mahmudmridul.185@gmail.com>
2024-01-18 10:53:11 +0600

    Pass meta data

--- src/components/ListItem.jsx
+++ src/components/ListItem.jsx
@@ -4,7 +4,7 @@ import { Link } from "react-router-dom";
 export default function ListItem({ blog }) {
    return (
       <li className="bg-zinc-100 p-2 mt-2 rounded-md hover:bg-zinc-200">
-         <Link to={`blogs/${blog.fileName}`}>
+         <Link to={`blogs/${blog.fileName}`} state={{ info: blog }}>
             <h6 className="">{blog.name}</h6>
             <div> Category: {blog.category} </div>
             <div> Published: {blog.published} </div>
